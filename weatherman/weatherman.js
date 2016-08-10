bdc87dda8b724db289d3fc4f10cb723992c46fb4
Evan Yip <ehhy69@gmail.com>
2016-08-10 11:21:46 +0800

    updated weatherman files

--- weatherman/weatherman.js
+++ weatherman/weatherman.js
@@ -1,15 +1,15 @@
 var weather = prompt("Welcome to the weatherman. I will light up your day and do wahtever the weather suggests. What's the weather now?");
 weather = weather.toLowerCase();
 if (weather == "sunny") {
-	document.getElementById("animation").src="//giphy.com/embed/xT0Gqz4x4eLd5gDtaU" width="480" height="347" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>"";
+	document.getElementById("animation").src="//giphy.com/embed/xT0Gqz4x4eLd5gDtaU";
 } else if (weather == "rainy") {
-	document.getElementById("animation").src="//giphy.com/embed/Of5eQwAXwTpE4" width="480" height="379" frameBorder="0" class="giphy-embed" allowFullScreen>";
+	document.getElementById("animation").src="//giphy.com/embed/Of5eQwAXwTpE4";
 } else if (weather == "windy") {
-	document.getElementById("animation").src="//giphy.com/embed/5RHAxkNRlLPjy" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen>";
+	document.getElementById("animation").src="giphy.com/embed/5RHAxkNRlLPjy";
 } else if (weather == "cloudy") {
-	document.getElementById("animation").src="";
+	document.getElementById("animation").src="//giphy.com/embed/y3xjJsIg3Mq3K";
 } else if (weather == "snowy") {
-	document.getElementById("animation").src="";
+	document.getElementById("animation").src="//giphy.com/embed/I2sBuEE64SuMo";
 } else {
-	document.getElementById("animation").src="";	
+	document.getElementById("animation").src="//giphy.com/embed/OcbTkg1u5N2E";	
 }
