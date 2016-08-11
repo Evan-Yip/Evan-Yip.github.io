a131a66c8a2628fcb30c327e0a1fd32c9b78c894
Evan Yip <ehhy69@gmail.com>
2016-08-11 10:23:11 +0800

    updated

--- rps/rps.js
+++ rps/rps.js
@@ -1,4 +1,5 @@
-var userChoice = prompt("What is your move!");
+// var userChoice = prompt("What is your move!");
+var userChoice = document.getElementById("rpsForm").elements[0].value;
 userChoice = userChoice.toLowerCase();
 
 var computerChoice;
@@ -12,16 +13,25 @@ if (userChoice == computerChoice) {
 
 } else if (userChoice == "rock" && computerChoice == "scissors") {
 	document.getElementById("result").innerHTML = "The user wins!";
+	alert("The user wins!")
 } else if (userChoice == "rock" && computerChoice == "paper") {
 	document.getElementById("result").innerHTML = "The Computer Wins!";
+	alert("The computer wins!")
 } else if (userChoice == "paper" && computerChoice == "rock") {
 	document.getElementById("result").innerHTML = "The user wins!";
+	alert("The user wins!")
 } else if (userChoice == "paper" && computerChoice == "scissors") {
 	document.getElementById("result").innerHTML = "The Computer Wins!";
+	alert("The computer wins!")
 } else if (userChoice == "scissors" && computerChoice == "paper") {
 	document.getElementById("result").innerHTML = "The user wins!";
+	alert("The user wins!")
 } else if (userChoice == "scissors" && computerChoice == "rock") {
 	document.getElementById("result").innerHTML = "The Computer Wins!";
+	alert("The computer wins!")
 } else {
 	alert("Invalid Input!");
+}
+function playAgain() {
+	window.location.reload();
 }
