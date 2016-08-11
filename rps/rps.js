09e2abca0fc710301c264b081c43f8890c125a1c
Evan Yip <ehhy69@gmail.com>
2016-08-11 11:06:22 +0800

    deb-g

--- rps/rps.js
+++ rps/rps.js
@@ -1,39 +1,37 @@
 function play() {
-	// var userChoice = prompt("What is your move!");
-	var userChoice = document.getElementById("rpsForm").elements[0].value;
-	userChoice = userChoice.toLowerCase();
-
-	var computerChoice;
-
+	var form = document.getElementById("rpsForm");
+	var userChoice = form.elements[0].value;
 	var options = ["rock", "paper", "scissors"];
 	var randomNumber = Math.floor(Math.random() * 3);
+	var computerChoice = options[randomNumber];
 
-	computerChoice = options[randomNumber];
-
+	alert("The computers choice is " + computerChoice);
 	if (userChoice == computerChoice) {
-
+		document.getElementById("result").innerHTML = "It's a tie!";
+		alert("It's a tie!");
 	} else if (userChoice == "rock" && computerChoice == "scissors") {
 		document.getElementById("result").innerHTML = "The user wins!";
-		alert("The user wins!")
+		alert("The user wins!");
 	} else if (userChoice == "rock" && computerChoice == "paper") {
 		document.getElementById("result").innerHTML = "The Computer Wins!";
-		alert("The computer wins!")
+		alert("The computer wins!");
 	} else if (userChoice == "paper" && computerChoice == "rock") {
 		document.getElementById("result").innerHTML = "The user wins!";
-		alert("The user wins!")
+		alert("The user wins!");
 	} else if (userChoice == "paper" && computerChoice == "scissors") {
 		document.getElementById("result").innerHTML = "The Computer Wins!";
-		alert("The computer wins!")
+		alert("The computer wins!");
 	} else if (userChoice == "scissors" && computerChoice == "paper") {
 		document.getElementById("result").innerHTML = "The user wins!";
-		alert("The user wins!")
+		alert("The user wins!");
 	} else if (userChoice == "scissors" && computerChoice == "rock") {
 		document.getElementById("result").innerHTML = "The Computer Wins!";
-		alert("The computer wins!")
+		alert("The computer wins!");
 	} else {
 		alert("Invalid Input!");
 	}
-	function playAgain() {
-		window.location.reload();
-	}
+}
+
+function playAgain() {
+	window.location.reload();
 }
