cf0a1b9fa2bf90a234a9f6817f8a2854a8dc2ac7
Evan Yip <ehhy69@gmail.com>
2016-08-11 10:52:58 +0800

    update 2

--- rps/rps.js
+++ rps/rps.js
@@ -1,37 +1,39 @@
-// var userChoice = prompt("What is your move!");
-var userChoice = document.getElementById("rpsForm").elements[0].value;
-userChoice = userChoice.toLowerCase();
+function play() {
+	// var userChoice = prompt("What is your move!");
+	var userChoice = document.getElementById("rpsForm").elements[0].value;
+	userChoice = userChoice.toLowerCase();
 
-var computerChoice;
+	var computerChoice;
 
-var options = ["rock", "paper", "scissors"];
-var randomNumber = Math.floor(Math.random() * 3);
+	var options = ["rock", "paper", "scissors"];
+	var randomNumber = Math.floor(Math.random() * 3);
 
-computerChoice = options[randomNumber];
+	computerChoice = options[randomNumber];
 
-if (userChoice == computerChoice) {
+	if (userChoice == computerChoice) {
 
-} else if (userChoice == "rock" && computerChoice == "scissors") {
-	document.getElementById("result").innerHTML = "The user wins!";
-	alert("The user wins!")
-} else if (userChoice == "rock" && computerChoice == "paper") {
-	document.getElementById("result").innerHTML = "The Computer Wins!";
-	alert("The computer wins!")
-} else if (userChoice == "paper" && computerChoice == "rock") {
-	document.getElementById("result").innerHTML = "The user wins!";
-	alert("The user wins!")
-} else if (userChoice == "paper" && computerChoice == "scissors") {
-	document.getElementById("result").innerHTML = "The Computer Wins!";
-	alert("The computer wins!")
-} else if (userChoice == "scissors" && computerChoice == "paper") {
-	document.getElementById("result").innerHTML = "The user wins!";
-	alert("The user wins!")
-} else if (userChoice == "scissors" && computerChoice == "rock") {
-	document.getElementById("result").innerHTML = "The Computer Wins!";
-	alert("The computer wins!")
-} else {
-	alert("Invalid Input!");
-}
-function playAgain() {
-	window.location.reload();
+	} else if (userChoice == "rock" && computerChoice == "scissors") {
+		document.getElementById("result").innerHTML = "The user wins!";
+		alert("The user wins!")
+	} else if (userChoice == "rock" && computerChoice == "paper") {
+		document.getElementById("result").innerHTML = "The Computer Wins!";
+		alert("The computer wins!")
+	} else if (userChoice == "paper" && computerChoice == "rock") {
+		document.getElementById("result").innerHTML = "The user wins!";
+		alert("The user wins!")
+	} else if (userChoice == "paper" && computerChoice == "scissors") {
+		document.getElementById("result").innerHTML = "The Computer Wins!";
+		alert("The computer wins!")
+	} else if (userChoice == "scissors" && computerChoice == "paper") {
+		document.getElementById("result").innerHTML = "The user wins!";
+		alert("The user wins!")
+	} else if (userChoice == "scissors" && computerChoice == "rock") {
+		document.getElementById("result").innerHTML = "The Computer Wins!";
+		alert("The computer wins!")
+	} else {
+		alert("Invalid Input!");
+	}
+	function playAgain() {
+		window.location.reload();
+	}
 }
